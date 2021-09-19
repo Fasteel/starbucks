8c92a1f8c7ffcc32c2a7d0a0e9d49ed65ffe3329
Carl MONNERA <fasteel.web@gmail.com>
2021-09-19 23:06:17 +0200

    :lipstick: Add active option to NavigationItem

--- src/stories/NavigationItem.stories.tsx
+++ src/stories/NavigationItem.stories.tsx
@@ -5,6 +5,13 @@ import NavigationItem from "./NavigationItem";
 export default {
   title: `${APP_NAME}/NavigationItem`,
   component: NavigationItem,
+  decorators: [
+    (Story) => (
+      <div style={{ height: "100px", display: "inline-block" }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof NavigationItem>;
 
 const Template: ComponentStory<typeof NavigationItem> = (args) => (
@@ -14,4 +21,10 @@ const Template: ComponentStory<typeof NavigationItem> = (args) => (
 export const Base = Template.bind({});
 Base.args = {
   label: "Menu",
+};
+
+export const Active = Template.bind({});
+Active.args = {
+  label: "Menu",
+  active: true,
 };
--- src/stories/NavigationItem.tsx
+++ src/stories/NavigationItem.tsx
@@ -4,9 +4,10 @@ import theme from "./theme";
 type NavigationItemProps = {
   label?: string;
   onClick?: () => void;
+  active?: boolean;
 };
 
-const Wrapper = styled.li`
+const Wrapper = styled.li<{ active?: boolean }>`
   list-style: none;
   font-family: "${theme.fonts.primary}", sans-serif;
   font-weight: 800;
@@ -14,21 +15,25 @@ const Wrapper = styled.li`
   cursor: pointer;
   user-select: none;
   margin-right: 2.4rem;
+  height: 100%;
+  ${({ active }) => active && `border-bottom: 5px solid ${theme.colors.green};`}
 `;
 
 const Link = styled.a`
-  display: inline-block;
+  display: flex;
+  align-items: center;
   color: rgb(31, 31, 31);
   letter-spacing: 0.12rem;
   transition: color 0.5s;
   font-size: 0.9rem;
+  height: 100%;
   &:hover {
     color: ${theme.colors.greenDarker};
   }
 `;
 
-const NavigationItem = ({ label, onClick }: NavigationItemProps) => (
-  <Wrapper onClick={onClick}>
+const NavigationItem = ({ label, onClick, active }: NavigationItemProps) => (
+  <Wrapper onClick={onClick} active={active}>
     <Link>{label}</Link>
   </Wrapper>
 );

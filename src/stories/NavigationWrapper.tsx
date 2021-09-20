20835f1132833f2abf32ab4faad8144e0d07e3a3
Carl MONNERA <fasteel.web@gmail.com>
2021-09-20 23:43:27 +0200

    :lipstick: Bottom NavBar

--- src/stories/NavigationWrapper.tsx
+++ src/stories/NavigationWrapper.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 const NavigationWrapper = styled.div`
+  background: #fff;
+  box-shadow: 0 0.2rem 0.2rem 0 rgb(0 0 0 / 12%);
   height: 100px;
   display: flex;
   align-items: center;
